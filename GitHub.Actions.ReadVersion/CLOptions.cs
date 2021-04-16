875af5324a9980b6e80b8ad33557135bbc4b762d
Jason Davis <jasondavis303@gmail.com>
2021-04-16 08:48:27 -0600

    Add project files.

---
+++ GitHub.Actions.ReadVersion/CLOptions.cs
@@ -0,0 +1,17 @@
+﻿using CommandLine;
+
+namespace GitHub.Actions.ReadVersion
+{
+    class CLOptions
+    {
+        [Option("from-file", Required = true)]
+        public string FromFile { get; set; }
+
+        [Option("out-var", Default = "READ_VERSION")]
+        public string OutputVariable { get; set; }
+
+        [Option("env-file")]
+        public string EnvironmentFile { get; set; }
+
+    }
+}
---
+++ GitHub.Actions.ReadVersion/Program.cs
@@ -0,0 +1,51 @@
+﻿using CommandLine;
+using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace GitHub.Actions.ReadVersion
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            int ret = -1;
+
+            try
+            {
+                Parser.Default.ParseArguments<CLOptions>(args)
+                    .WithParsed(opts =>
+                    {
+                        Run(opts);
+                        ret = 0;
+                    });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ResetColor();
+                Console.WriteLine();
+
+                if (ex.HResult != 0)
+                    ret = ex.HResult;
+            }
+
+            return ret;
+        }
+
+        static void Run(CLOptions opts)
+        {
+            var doc = XDocument.Load(opts.FromFile);
+            var version = new Version(doc.Root.Descendants("Version").First().Value);
+            Console.Out.WriteLine($"::set-output name={opts.OutputVariable}::{version}");
+            if(!string.IsNullOrWhiteSpace(opts.EnvironmentFile))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(opts.EnvironmentFile));
+                File.AppendAllLines(opts.EnvironmentFile, new string[] { $"{opts.OutputVariable}={version}" });
+            }
+        }
+    }
+}
